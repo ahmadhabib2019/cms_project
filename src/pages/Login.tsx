88c490185d549a9df916161cb4705a926a3d422a
ahmadhabib2019 <ahmad.habib.m13@gmail.com>
2025-03-03 19:44:21 -0500

    add welcome mediverse

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -52,7 +52,7 @@ const Login: React.FC = () => {
       <img src={logomediverse} className="logo-mediverse" />
       </div>
       <div className="login-left">
-        <h2>Selamat Datang</h2>
+        <h2>Welcome to mediverse</h2>
         <p>Masuk dan kelola dashboard Mediverse Anda</p>
         <p></p>
         <form onSubmit={handleLogin}>
